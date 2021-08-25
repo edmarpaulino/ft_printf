f53350c1d731974cbd4c5842ec887b91ada1a4cf
Edmar Paulino <edmarpaulino9@gmail.com>
2021-08-25 18:50:07 -0300

    remove ft_hex_address

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: edpaulin <edpaulin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/18 15:49:40 by edpaulin          #+#    #+#             */
-/*   Updated: 2021/08/25 18:29:14 by edpaulin         ###   ########.fr       */
+/*   Updated: 2021/08/25 18:49:12 by edpaulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@
 size_t	ft_strlen(char	*str);
 int		ft_nbrlen(size_t	nbr, size_t	base);
 char	*ft_uitoa(unsigned int	un);
-char	*ft_hex_address(size_t	addr, char	*base);
 char	*ft_convert_base(size_t	nbr, char	*base);
 
 #endif
